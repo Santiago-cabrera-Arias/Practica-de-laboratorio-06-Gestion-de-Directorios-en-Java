88717335e904f1d8ac213044f447795af3329ee9
Santiago-cabrera-Arias <santiagocabrera354@gmail.com>
2020-07-06 00:41:56 -0500

    Actualizacion del metodo eliminar Directorio

--- src/ec/edu/ups/controladorDirectorio/ControladorDirectorio.java
+++ src/ec/edu/ups/controladorDirectorio/ControladorDirectorio.java
@@ -33,8 +33,8 @@ public class ControladorDirectorio {
     }
 
     //Eliminar Directorio
-    public void eliminar(String EliminarArchivo) {
-        archivo = new File(EliminarArchivo);
+    public void eliminar(String eliminarDirectorio) {
+        archivo = new File(eliminarDirectorio);
         File[] archivosLista = archivo.listFiles();
         if (archivo.isDirectory()) {
             for (int i = 0; i < archivosLista.length; i++) {
